d951dea4b0925ac7813164c602da7fa01bfded38
Jobu <111178322+Job-u@users.noreply.github.com>
2026-02-13 14:29:40 +0800

    Implement image processing functions: grayscale, reflect, blur, edges

--- week4/filter-more/helpers.c
+++ week4/filter-more/helpers.c
@@ -1,25 +1,145 @@
 #include "helpers.h"
+#include <math.h>
+#include <stdio.h>
 
-// Convert image to grayscale
+
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+            int avg = round((image[i][j].rgbtRed +
+                             image[i][j].rgbtGreen +
+                             image[i][j].rgbtBlue) / 3.0);
+
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
 }
 
-// Reflect image horizontally
+
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = temp;
+        }
+    }
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sumRed = 0, sumGreen = 0, sumBlue = 0;
+            int count = 0;
+
+            // Iterate over neighboring pixels (3x3)
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
+                        sumRed += copy[ni][nj].rgbtRed;
+                        sumGreen += copy[ni][nj].rgbtGreen;
+                        sumBlue += copy[ni][nj].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+
+            image[i][j].rgbtRed = round(sumRed / (float)count);
+            image[i][j].rgbtGreen = round(sumGreen / (float)count);
+            image[i][j].rgbtBlue = round(sumBlue / (float)count);
+        }
+    }
 }
 
-// Detect edges
+
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    RGBTRIPLE copy[height][width];
+
+
+    for (int i = 0; i < height; i++)
+        for (int j = 0; j < width; j++)
+            copy[i][j] = image[i][j];
+
+    // Sobel kernels
+    int Gx[3][3] = {
+        {-1, 0, 1},
+        {-2, 0, 2},
+        {-1, 0, 1}
+    };
+    int Gy[3][3] = {
+        {-1, -2, -1},
+        {0, 0, 0},
+        {1, 2, 1}
+    };
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sumRx = 0, sumGx = 0, sumBx = 0;
+            int sumRy = 0, sumGy = 0, sumBy = 0;
+
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
+                        int kx = Gx[di + 1][dj + 1];
+                        int ky = Gy[di + 1][dj + 1];
+
+                        sumRx += copy[ni][nj].rgbtRed * kx;
+                        sumGx += copy[ni][nj].rgbtGreen * kx;
+                        sumBx += copy[ni][nj].rgbtBlue * kx;
+
+                        sumRy += copy[ni][nj].rgbtRed * ky;
+                        sumGy += copy[ni][nj].rgbtGreen * ky;
+                        sumBy += copy[ni][nj].rgbtBlue * ky;
+                    }
+                }
+            }
+
+            // Calculate gradient magnitude
+            int red = round(sqrt(sumRx * sumRx + sumRy * sumRy));
+            int green = round(sqrt(sumGx * sumGx + sumGy * sumGy));
+            int blue = round(sqrt(sumBx * sumBx + sumBy * sumBy));
+
+            // Cap at 255
+            image[i][j].rgbtRed = red > 255 ? 255 : red;
+            image[i][j].rgbtGreen = green > 255 ? 255 : green;
+            image[i][j].rgbtBlue = blue > 255 ? 255 : blue;
+        }
+    }
 }
