9ccf9e27e9b0fa7f3a581d5e992e7f2cc0b8bef6
Jobu <111178322+Job-u@users.noreply.github.com>
2026-02-13 14:14:33 +0800

    Add SQL queries for July 28, 2025 investigations
    Added SQL queries to log for various investigations on July 28, 2025, including crime scene reports, witness interviews, ATM withdrawals, and flight checks.

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,2 +1,101 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Date: July 28, 2025, Location: Humphrey Street
+
+Location: Humphrey Street
+SELECT description
+FROM crime_scene_reports
+WHERE year = 2025
+  AND month = 7
+  AND day = 28
+  AND street = 'Humphrey Street';
+
+-- Check Witness Interviews
+
+SELECT name, transcript
+FROM interviews
+WHERE year = 2025
+  AND month = 7
+  AND day = 28;
+
+-- Check ATM Withdrawals
+
+SELECT *
+FROM atm_transactions
+WHERE year = 2025
+  AND month = 7
+  AND day = 28
+  AND atm_location = 'Humphrey Street'
+  AND transaction_type = 'withdraw';
+
+-- link account numbers to people:
+
+SELECT people.name
+FROM people
+JOIN bank_accounts
+ON people.id = bank_accounts.person_id
+WHERE bank_accounts.account_number IN (
+   -- account numbers
+);
+
+-- Check Bakery Security Logs
+
+SELECT *
+FROM bakery_security_logs
+WHERE year = 2025
+  AND month = 7
+  AND day = 28
+  AND hour = 10;
+
+-- match license plates:
+SELECT name
+FROM people
+WHERE license_plate IN (
+    --license plates from logs
+);
+
+-- Check Flights Out of Town
+
+SELECT id
+FROM airports
+WHERE city = 'Fiftyville';
+
+-- earliest flight next day:
+SELECT *
+FROM flights
+WHERE year = 2025
+  AND month = 7
+  AND day = 29
+  AND origin_airport_id = (
+      SELECT id FROM airports WHERE city = 'Fiftyville'
+  )
+ORDER BY hour, minute
+LIMIT 1;
+
+-- City
+
+SELECT city
+FROM airports
+WHERE id = -- desti airport id;
+
+-- Peoples That Were On The Flight
+
+SELECT people.name
+FROM passengers
+JOIN people
+ON passengers.passport_number = people.passport_number
+WHERE flight_id = -- flight id;
+
+-- Accomplice
+
+SELECT *
+FROM phone_calls
+WHERE year = 2025
+  AND month = 7
+  AND day = 28
+  AND duration < 60;
+
+
+SELECT name
+FROM people
+WHERE phone_number = --number;
